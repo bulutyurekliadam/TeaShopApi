d82904a91470631ee0962f903f023f4ba612a405
bulutyurekliadam <ayvaz.hakan@outlook.com>
2023-12-04 02:06:31 +0300

    Add project files.

---
+++ TeaShopApi.DataAccessLayer/Context/TeaContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeaShopApi.EntityLayer.Concrete;
+
+namespace TeaShopApi.DataAccessLayer.Context
+{
+    public class TeaContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-H67U406;initial catalog = TeaShopDb; integrated Security = true;");
+        }
+        public DbSet<Drink>Drinks { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Question> Questions{ get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+    }
+}
---
+++ TeaShopApi.DataAccessLayer/Migrations/20231203230332_mig_first2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeaShopApi.DataAccessLayer.Migrations
+{
+    public partial class mig_first2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DringImageUrl",
+                table: "Drinks",
+                newName: "DrinkImageUrl");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DrinkImageUrl",
+                table: "Drinks",
+                newName: "DringImageUrl");
+        }
+    }
+}
